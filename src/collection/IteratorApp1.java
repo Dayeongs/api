7a648d62a04b054595e1be6c3c28ba269453d51b
Dayeongs <ekdud9636@gmail.com>
2023-11-17 17:15:19 +0900

    Iterator 실습

---
+++ src/collection/IteratorApp1.java
@@ -0,0 +1,38 @@
+package collection;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorApp1 {
+	
+	public static void main(String[] args) {
+		
+		List<String> list = Arrays.asList("홍길동", "김유신", "강감찬", "이순신", "류관순");
+		
+		System.out.println("### 일반 for문을 사용해서 반복처리하기 - List 계열만 가능");
+		int size = list.size();
+		// for (int index = 0; index < list.size(); index++) 사용X
+		for (int index = 0; index < size; index++) {
+			String name = list.get(index);
+			System.out.println(name);
+		}
+		
+		System.out.println("### 향상된 for문을 사용해서 반복처리하기");
+		for (String name : list) {
+			System.out.println(name);
+		}
+		
+		System.out.println("### Iterator를 사용해서 반복처리하기");
+		Iterator<String> itr = list.iterator();
+		while (itr.hasNext()) {			// hashNext()로 반복할 요소가 남아있는지 확인하기
+			String name = itr.next();	// next()로 요소 꺼내기
+			System.out.println(name);
+		}
+		
+		System.out.println("### Stream를 사용해서 반복처리하기");
+		list.stream().forEach(name -> System.out.println(name));
+		
+	}
+	
+}
